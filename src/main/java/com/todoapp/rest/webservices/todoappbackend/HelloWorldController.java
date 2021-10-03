0b1a6f11e0ec1b5d158421529475ef2d03ff7993
noatorious <noatimisela@gmail.com>
2021-10-03 15:47:27 +0200

    Add bean class and GET method for path variable

--- src/main/java/com/todoapp/rest/webservices/todoappbackend/HelloWorldController.java
+++ src/main/java/com/todoapp/rest/webservices/todoappbackend/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.todoapp.rest.webservices.todoappbackend;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Controller
 @RestController
@@ -10,8 +8,20 @@ public class HelloWorldController {
     //GET method= helloWorld
     //URI path= "/hello-world"
 
-    @RequestMapping(method= RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    //hello-world-bean/path-variable/noaki
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorlPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
