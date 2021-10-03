140e64232adf0e117ea9eae278ab16f0ac860134
noatorious <noatimisela@gmail.com>
2021-10-03 13:20:18 +0200

    Init commit

---
+++ src/test/java/com/todoapp/rest/webservices/todoappbackend/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoapp.rest.webservices.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
